f7745e6a953be56a2db5079d65910198f85a4e7c
OBRIEN66 <nicholas.obrien@ucc.on.ca>
2017-12-12 21:40:28 -0500

    Readme

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # project-finder
-A website to find projects that want help
+
+A website to find projects that want help.
+
+```bash
+git clone https://github.com/obrien66/project-finder
+cd project-finder
+npm install
+npm run config # if you want a port other than 8000
+npm start
+```
--- index.js
+++ index.js
@@ -5,8 +5,9 @@ require("dotenv").config()
 var http = require("http");
 var handler = require("./src/handler")
 
+var port = process.env.PORT || 8000
 // Initialize and run server on process.env.PORT
 var server = http.createServer(handler)
-server.listen(process.env.PORT)
+server.listen(port)
 
-console.log("http://localhost:" + process.env.PORT)
+console.log("http://localhost:" + port)
